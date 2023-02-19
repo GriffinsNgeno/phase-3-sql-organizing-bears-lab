d4facdab70b89a2b57ca381d584f63f68ae99114
GriffinsNgeno <griffins.ngeno@student.moringaschool.com>
2023-02-19 14:06:23 +0300

    Completed Lab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 12, 'M', 'brown', 'social', 0);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ('Rowdy', 12, 'M', 'peach', 'social', 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ('Tabitha', 12, 'F', 'orange', 'social', 0);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 12, 'M', 'black', 'social', 0);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ('Melissa', 12, 'F', 'pink', 'social', 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ('Grinch', 12, 'M', 'yellow', 'social', 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ('Wendy', 12, 'F', 'white', 'social', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES(NULL, 15, 'M', 'blue', 'sad', 1);
